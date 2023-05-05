1832195d3f36678e47a99ef7426270d7b00cb3fe
valerianCapon <valerian.capon@laposte.net>
2023-05-05 16:47:56 +0100

    advancing on models

--- CTJTest_WebSite/apps/colorTestApp/models.py
+++ CTJTest_WebSite/apps/colorTestApp/models.py
@@ -2,46 +2,64 @@ from django.db import models
 from colorfield.fields import ColorField
 from django.contrib.auth.models import User
 
+class ColorTest(models.Model):
+    TYPE_OF_TEST = (
+        ('r', 'Rubric'),
+        ('a', 'ACJ'),
+        ('t', 'CTJ'),
+    )
+    type = models.CharField(
+        max_length=1,
+        choices=TYPE_OF_TEST,
+        blank=True,
+        default='r',
+    )
 
-# class Test(models.Model):
-#     type = models.CharField(max_length=8)
-#     judge =
-#     date_started
-#     date_ended
-#     duration
-
-#     TYPE_OF_TEST = (
-#         ('r', 'Rubric'),
-#         ('a', 'ACJ'),
-#         ('t', 'CTJ'),
-#     )
-
-#     status = models.CharField(
-#         max_length=1,
-#         choices=LOAN_STATUS,
-#         blank=True,
-#         default='m',
-#         help_text='Book availability',
-#     )
-
-
-# class ACJAssessment(models.Model)
-#     test = models.ForeignKey()
-#     color = models.ForeignKey()
-#     shade_guessed = 
-#     time_start
-#     time_end
-
-
-# class ColorSet(models.Model):
-#     colors = models.ManyToManyField(Color)
-#     quantityOfColors = 
-#     name = 
-#     description = 
-
-
-# class Color(models.Model):
-#     color_code = ColorField(default='#00FF00')
-#     color_set = models.CharField(max_length=255)
-#     rank_in_set
+    judge = models.OneToOneField(User, on_delete=models.CASCADE, related_name="is_user")
     
+    #auto_now_add=True mean the date is saved when the object is created only
+    date_started = models.DateTimeField(auto_now_add=True) 
+    #auto_now=True mean the date is saved everytime the object is saved 
+    date_ended = models.DateTimeField(auto_now=True)
+
+    duration = models.DurationField()
+    
+    #name = "Date + Type + Judge
+    name = models.CharField(max_length=200, unique=True)
+
+    def __str__(self) -> str:
+        return self.name
+    
+
+
+class Color(models.Model):
+    color_code = ColorField(default='#00FF00', unique=True)
+    name = models.CharField(max_length=20)
+
+    def __str__(self) -> str:
+        return self.name
+
+    
+
+#Rubric Assessment
+class RubricAssmt(models.Model):
+    test = models.ForeignKey(ColorTest, on_delete=models.CASCADE, related_name="of_test")
+    color = models.ForeignKey(Color)
+    shade_guessed = models.IntegerField()
+    time_start = models.DateField(auto_now=True)
+    time_end = models.DateField(auto_now=True)
+
+    #name = test.name + color
+    name = models.CharField(max_length=200, unique=True)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+
+class ColorSet(models.Model):
+    colors = models.ManyToManyField(Color)
+    quantityOfColors = models.IntegerField(min_value=0)
+    name = 
+    description = 
+
