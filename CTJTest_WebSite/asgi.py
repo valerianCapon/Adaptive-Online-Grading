58d4323ade4628072d84037e6cf6da8a1c6cd876
valerianCapon <valerian.capon@laposte.net>
2023-05-05 14:21:30 +0100

    change name from TCJ to CTJ

--- CTJTest_WebSite/asgi.py
+++ CTJTest_WebSite/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for TCJTest_WebSite project.
+ASGI config for CTJTest_WebSite project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TCJTest_WebSite.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CTJTest_WebSite.settings')
 
 application = get_asgi_application()
--- CTJTest_WebSite/wsgi.py
+++ CTJTest_WebSite/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for TCJTest_WebSite project.
+WSGI config for CTJTest_WebSite project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TCJTest_WebSite.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CTJTest_WebSite.settings')
 
 application = get_wsgi_application()
